8bdb32b9536444eac67e7d090b20156dd63e1b8b
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-03-02 14:06:22 +0200

    Initialise redux logic for missions data

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,9 +1,80 @@
-// const url = 'https://api.spacexdata.com/v3/rockets';
-// const GET_ROCKETS = 'GET_ROCKETS';
-// const RESERVE_ROCKET = 'RESERVE_ROCKETS';
-// const FETCHING_ROCKETS_FAILED = 'FETCHING_ROCKETS_FAILED';
-// const CANCEL_RESERVATION = 'CANCEL_RESERVATION';
+const url = 'https://api.spacexdata.com/v3/missions';
+const GET_MISSIONS = 'GET_MISSIONS';
+const FETCHING_MISSIONS_FAILED = 'FETCHING_ROCKETS_FAILED';
+const CHANGE_MISSION_STATUS = 'CHANGE_MISSION_STATUS';
+// const LEAVE_MISSION = 'LEAVE_MISSION';
 
-// const initialState = {
-//   rockets: [],
-// };
+const initialState = {
+  missions: [],
+};
+
+const loadMissions = (missions) => ({
+  type: GET_MISSIONS,
+  payload: missions,
+});
+
+const fetchingDataFailed = (err) => ({
+  type: FETCHING_MISSIONS_FAILED,
+  payload: err,
+});
+
+export const changeMissionStatus = (id) => ({
+  type: CHANGE_MISSION_STATUS,
+  payload: id,
+});
+
+export const fetchMissions = () => async (dispatch) => {
+  try {
+    const response = await fetch(url);
+    const missions = await response.json();
+
+    dispatch(
+      loadMissions(
+        missions.map((mission) => {
+          const {
+            mission_id: id,
+            mission_name: missionName,
+            description,
+          } = mission;
+          const newMissionDerivedDetails = {
+            id, missionName, description, isUserJoinedToMission: false,
+          };
+          return newMissionDerivedDetails;
+        }),
+      ),
+    );
+  } catch (err) {
+    err.description = 'An error occurred, please try again later';
+    dispatch(fetchingDataFailed(err.description));
+  }
+};
+
+const missionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_MISSIONS:
+      return {
+        ...state,
+        missions: action.payload,
+      };
+
+    case FETCHING_MISSIONS_FAILED:
+      return {
+        ...state,
+        // loading: false,
+        error: action.payload,
+      };
+    case CHANGE_MISSION_STATUS: {
+      const newState = state.rockets.map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, isUserJoinedToMission: !item.isUserJoinedToMission };
+        }
+        return item;
+      });
+      return { ...state, missions: newState };
+    }
+    default:
+      return state;
+  }
+};
+
+export default missionReducer;

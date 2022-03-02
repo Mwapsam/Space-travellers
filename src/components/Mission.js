899bb8251c601ee542a95bf4a125f81f1c0079d3
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-03-02 12:30:33 +0200

    Create Mission draft static UI

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,7 +1,68 @@
 import React from 'react';
+import styles from './styles/Mission.module.css';
+
+const missions = [{
+  mission: 'MISSION 1',
+  description: `Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem 
+ Ipsum Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+ Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum`,
+  isUserJoinedToMission: false,
+}, {
+  mission: 'MISSION 2',
+  description: `Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem 
+  Ipsum Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+  Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum`,
+  isUserJoinedToMission: false,
+}, {
+  mission: 'MISSION 3',
+  description: `Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem 
+  Ipsum Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+  Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum`,
+  isUserJoinedToMission: true,
+}, {
+  mission: 'MISSION 4',
+  description: `Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem 
+  Ipsum Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+  Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum`,
+  isUserJoinedToMission: false,
+}];
 
 const Mission = () => (
-  <div>Mission</div>
+  <section className={styles.missionSectionContainer}>
+    <div className={styles.missionGridContainer}>
+      <div className={`${styles.headerText} ${styles.headerTextTopGridBlackBorder}`}>
+        MISSIONS
+      </div>
+      <div className={`${styles.headerText} ${styles.headerTextTopGridBlackBorder}`}>
+        DESCRIPTION
+      </div>
+      <div className={`${styles.headerText} ${styles.headerTextTopGridBlackBorder}`}>
+        STATUS
+      </div>
+      <div className={`${styles.headerText} ${styles.headerTextTopGridBlackBorder}`} />
+    </div>
+
+    {missions.map((element, index) => (
+      <div className={`${styles.missionGridContainer} ${index % 2 === 1 ? styles.missionGridGreyContainerColor : ''}`}>
+        <div className={styles.headerText}>
+          {element.mission}
+        </div>
+        <div className={styles.headerText}>
+          {element.description}
+        </div>
+        <div className={styles.headerText}>
+          {element.isUserJoinedToMission
+            ? <div className={styles.activeMemberButton}> ACTIVE MEMBER </div>
+            : <div className={styles.notActiveMemberButton}> NOT A MEMBER</div>}
+        </div>
+        <div className={styles.headerText}>
+          {element.isUserJoinedToMission
+            ? <button type="button" className={styles.joinMissionButton}> JOIN MISSION </button>
+            : <button type="button" className={styles.leaveMissionButton}> LEAVE MISSION </button>}
+        </div>
+      </div>
+    ))}
+  </section>
 );
 
 export default Mission;
---
+++ src/redux/missions/missions.js
@@ -0,0 +1,9 @@
+// const url = 'https://api.spacexdata.com/v3/rockets';
+// const GET_ROCKETS = 'GET_ROCKETS';
+// const RESERVE_ROCKET = 'RESERVE_ROCKETS';
+// const FETCHING_ROCKETS_FAILED = 'FETCHING_ROCKETS_FAILED';
+// const CANCEL_RESERVATION = 'CANCEL_RESERVATION';
+
+// const initialState = {
+//   rockets: [],
+// };
